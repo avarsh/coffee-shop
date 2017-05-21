81c28a5ef0539ecb4271e74a5b032404fbbbe684
Aporva Varshney <varshney.aporva@gmail.com>
2017-05-21 18:22:34 +0100

    Fixed compilation script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc `pkg-config --cflags gtk+-3.0` -o coffee-shop -lSDL_mixer -lSDL `pkg-config --libs gtk+-3.0` ./src/main.c ./src/interface.c ./src/audio.c ./src/file.c
+gcc `pkg-config --cflags gtk+-3.0` -o Coffee-Shop -lSDL_mixer -lSDL `pkg-config --libs gtk+-3.0` ./coffee-shop/main.c ./coffee-shop/interface.c ./coffee-shop/audio.c ./coffee-shop/file.c
